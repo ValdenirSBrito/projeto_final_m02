c925d85835af5ad78d6f72e4c0f3e47f99cc1801
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-10-16 19:41:03 -0400

    config deploy

--- index.js
+++ index.js
@@ -1,100 +1,155 @@
+require('dotenv').config()
 const express = require("express");
-const path = require("path");
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 3000;
-let message = "";
-
+const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-const pokedex = [];
+let message = "";
+
+// Esse é o responsável por trazer os dados.
+// Ele vai chamar os módulos da modelo e database para fazer a conexão e trazer
+// o objeto com todas as informações que precisamos.
+// Por isso que em todas as rotas que trabalhamos com nosso DB, vamos nos referir ao "Filmes"
+// Atenção com a letra maiúscula!!!!!
+const Filme = require("./models/filme");
 
 
-app.get("/", (req, res) => {
-  const devList = ["Backend", "Frontend", "Fullstack"];
-  const analyticsList = ["Engenharia de dados", "Ciência de dados"];
-  
-  setTimeout(() => {
-    message = "";
-  }, 1000);
 
+// R (Read) do CRUD - É aqui que vou LER os dados passados pelo banco
+app.get("/", async (req, res) => {
+  // Aqui estou pegando todas as entradas de "Filmes" e guardando em "filmes"
+  // Criando uma lista de objetos
+  const filmes = await Filme.findAll(); //find all = procurar tudo
+
+  // Renderizo minha página passando como variável o filmes que acabei de criar
+  // Ou seja, estou passando todos os dados puxados do BD
   res.render("index", {
-    titulo: "Blue",
-    devList: devList,
-    analyticsList: analyticsList,
-    pokedex: pokedex,
-    message,
+    filmes, message
   });
 });
 
-app.get("/cadastro", (req, res) => {
-    res.render("cadastro"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-});
-app.get("/detalhes", (req, res) => {
-    res.render("detalhes"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-});
-app.post("/new", (req, res) => {
-  const { numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade} = req.body;
-  const pokemon = ({ numero: numero, nome: nome, tipo: tipo, imagem: imagem, descricao: descricao, altura: altura, peso: peso, categoria: categoria, habilidade: habilidade });
-  pokedex.push(pokemon);
-  message = `Parabéns!!!! O Cadastro do Pokemon ${nome} foi realizada com sucesso!`;
-  res.redirect("/"); 
+
+// Read de uma entrada específica
+app.get("/detalhes/:id", async (req, res) => { //O id definido aqui na rota é passado pelo meu HTML quando clico no link
+  //Pegando uma entrada específica no banco (passada pelo ID) e construindo um objeto 
+  //Aqui é apenas um objeto, não uma lista como na rota principal.
+  const filme = await Filme.findByPk(req.params.id); //Find By PK - Procurar pela PK
+
+  res.render("detalhes", {
+    filme,
+  });
 });
 
 
-// Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.get("/criar", (req, res) => {
+  res.render("criar", {message});
+});
 
 
-// const express = require("express");
-// const path = require("path");
+// C (Create) do meu CRUD - Aqui eu vou criar uma entrada nova no meu banco
+app.post("/criar", async (req, res) => {
 
-// const app = express();
-// const port = process.env.PORT || 3000;
-// let message = "";
-// const pokedex = [];
+  const { nome, descricao, imagem } = req.body;
 
-// app.set("view engine", "ejs");
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.urlencoded());
+  if (!nome) {
+    res.render("criar", {
+      message: "Nome é obrigatório",
+    });
+  }
 
-// app.get("/", (req, res) => {
-//   setTimeout(() => {
-//     message = "";
-//   }, 1000);
+  else if (!imagem) {
+    res.render("criar", {
+      message: "Imagem é obrigatório",
+    });
+  }
 
-//   res.render("index", {
-//     titulo: "Pokédex",
-//     pokedex,
-//     message,
-//   });
-// });
+  else {
+    try {
+      const filme = await Filme.create({
+        nome,
+        descricao,
+        imagem,
+      });
+
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+
+      res.render("criar", {
+        message: "Ocorreu um erro ao cadastrar o Filme!",
+      });
+    }
+  }
+});
+
+// Embora a rota seja "editar", nesse momento, no GET, eu estou apenas pegando os dados
+// de uma entrada para serem editados, por isso ainda não é um UPDATE, é um READ.
+app.get("/editar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("editar", {
+      filme,
+      message: "Filme não encontrado!",
+    });
+  }
+
+  res.render("editar", {
+    filme, message
+  });
+});
+
+//U (Update) do meu CRUD - Aqui é onde eu faço a atualização (edição) dos dados de uma entrada
+app.post("/editar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  const { nome, descricao, imagem } = req.body;
+
+  filme.nome = nome;
+  filme.descricao = descricao;
+  filme.imagem = imagem;
+
+  const filmeEditado = await filme.save();
+
+  res.render("editar", {
+    filme: filmeEditado,
+    message: "Filme editado com sucesso!",
+  });
+});
 
 
-// app.post("/cadastro", (req, res) => {
-//     const { numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade} = req.body;
-//     const pokemon = ({numero:numero, nome:nome, tipo:tipo, imagem:imagem, descricao:descricao, altura:altura, peso:peso, categoria:categoria, habilidade:habilidade});
-//     pokedex.push(pokemon);
-//     res.render("cadastro",);
-// });
+app.get("/deletar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
 
-// // app.get("/detalhes", (req, res) => {
-// //     // const devList = ["Backend", "Frontend", "Fullstack"];
-// //     // const analyticsList = ["Engenharia de dados", "Ciência de dados"];
-// //     res.render("detalhes", { titulo: "Blue", devList: devList, analyticsList: analyticsList});
-// // });
+  if (!filme) {
+    res.render("deletar", {
+      filme,
+      message: "Filme não encontrado!",
+    });
+  }
 
-// // app.post("/subscription", (req, res) => {
-// //     const {nome, tipo, imagem} = req.body;
-// //     res.send("cadastro", {nome: nome, tipo: tipo, imagem: imagem});
-// //     message = (`Parabéns ${nome}, cadastrado com sucesso!`);
-// //     res.redirect("/");
-// // });
+  res.render("deletar", {
+    filme, message
+  });
+});
 
-// app.listen(port, () =>
-//   console.log(`Servidor rodando em http://localhost:${port}`)
-// );
+
+app.post("/deletar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("deletar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+
+  await filme.destroy();
+
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
---
+++ models/filme.js
@@ -0,0 +1,33 @@
+// Crio a modelo de como os dados serão trazidos do meu banco e tranformado em objetos
+
+const database = require("./../database");
+const Sequelize = require("sequelize");
+
+const Filme = database.define("filmes", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: Sequelize.STRING,
+  },
+  imagem: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+},
+{
+  freezeTableName: true,
+  timestamps: false, 
+  createdAt: false,
+  updatedAt: false,
+});
+
+module.exports = Filme;
+
